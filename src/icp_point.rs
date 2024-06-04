0c14c65e78e4307cdbf3f3804e3319587485bdb4
johannes-adr <john.rans@web.de>
2024-06-04 10:27:49 +0200

    Performacne change (2x faster)

--- src/icp_point.rs
+++ src/icp_point.rs
@@ -24,3 +24,24 @@ impl ICPPoint for na::Point2<f32>{
         true
     }
 }
+
+
+impl ICPPoint for (f32,f32){
+    fn translate(&self,x: f32,y: f32) -> Self {
+        let new = na::Point2::new(self.0+x,self.1+y);
+        (new.x,new.y)
+    }
+
+    fn rotate(&self,angle_rad: f32) -> Self {
+        let new = na::Rotation2::new(angle_rad) * self.point() ;
+        (new.x,new.y)
+    }
+
+    fn point(&self) -> na::Point2<f32> {
+        na::Point2::new(self.0,self.1)
+    }
+
+    fn is_data_valid(&self) -> bool {
+        true
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ mod icp_collection;
 use icp_collection::{ICPCollection, ICPCol, KDTreedIcpCollection};
 
 mod icp_point;
-use na::ComplexField;
 use nalgebra as na;
 
 
@@ -50,26 +49,26 @@ impl<'a,TRef: ICPPoint, TOther: ICPPoint> Icp<'a,TRef,TOther> {
     fn do_icp_generic(&mut self, x: f32, y: f32, angle_rad: f32, transformation_fn: fn(&mut KDTreedIcpCollection<TRef>, &mut ICPCollection<TOther>) -> (na::Vector2<f32>, f32)) -> ICPResult {
         let mut total_translation = na::Vector2::new(x, y);
         let mut total_rotation = angle_rad;
-    
+
         // Apply initial translation and rotation
         self.points_other.translate(x, y);
         self.points_other.rotate(angle_rad);
-    
+
         let mut i = 0;
         while i < self.max_iterations {
             // Calculate translation and rotation
             let (translation_vector, rotation) = transformation_fn(&mut self.points_reference, &mut self.points_other);
-    
+
             // Apply the calculated translation and rotation
             self.points_other.translate(translation_vector.x, translation_vector.y);
             self.points_other.rotate(rotation);
-    
+
             // Update total translation and rotation
             total_translation += translation_vector;
             total_rotation += rotation;
-    
+
             i += 1;
-    
+
             if translation_vector.norm() < self.convergence_distance && rotation.abs() < self.convergence_rotation {
                 break;
             }
@@ -83,48 +82,50 @@ impl<'a,TRef: ICPPoint, TOther: ICPPoint> Icp<'a,TRef,TOther> {
                 converged_count+=1;
             }
         }
-    
+
         ICPResult{x_offset: total_translation.x, y_offset: total_translation.y, rotation_offset_rad: total_rotation, convergence: converged_count as f32 / self.points_other.get_points().len() as f32 }
     }
 
 
     fn center_of_mass_corresp_kd_with_svd(scan1: &mut KDTreedIcpCollection<TRef>, scan2: &mut ICPCollection<TOther>) -> (na::Vector2<f32>, f32) {
         let n = scan2.get_points().len() as f32;
-    
+
         // Compute centroids of corresponding points, iterating over scan2
         let mut centroid1 = na::Point2::new(0.0, 0.0);
         let mut centroid2 = na::Point2::new(0.0, 0.0);
-        for point in scan2.get_points() {
-            let closest_point = scan1.closest_point_kd(point.point());
-            centroid1 += closest_point.coords;
-            centroid2 += point.point().coords;
+
+        let corresp = scan2.get_points().iter().map(|p|p.point()).map(|p|(p,scan1.closest_point_kd(p))).collect::<Vec<_>>();
+
+        for (point1,closest_point2) in &corresp{
+            centroid1 += closest_point2.coords;
+            centroid2 += point1.coords;
         }
+
         centroid1 /= n;
         centroid2 /= n;
-    
+
         // Construct the cross-covariance matrix, iterating over scan2
         let mut h = na::Matrix2::zeros();
-        for point in scan2.get_points() {
-            let closest_point = scan1.closest_point_kd(point.point());
-            let d1 = closest_point - centroid1;
-            let d2 = point.point() - centroid2;
+            for (point1,closest_point2) in &corresp{
+            let d1 = closest_point2 - centroid1;
+            let d2 = point1 - centroid2;
             h += d1 * d2.transpose();
         }
-    
+
         // Perform SVD
         let svd = na::SVD::new(h, true, true);
         let u = svd.u.unwrap();
         let vt = svd.v_t.unwrap();
-    
+
         // Compute rotation matrix
         let rotation_matrix = u * vt;
-    
+
         // Extract the rotation angle from the rotation matrix
         let rotation_angle = rotation_matrix[(1, 0)].atan2(rotation_matrix[(0, 0)]);
-    
+
         // Translation vector, adjusted to match the fast variant
         let translation_vector = centroid1 - rotation_matrix * centroid2;
-    
+
         (translation_vector.into(), rotation_angle)
     }
 
@@ -204,8 +205,8 @@ mod test{
     fn parse_and_plot(name: &str, func: impl Fn(&mut Scan, &mut Scan)->[f32;4]) {
         let mut scan1 = parse_scan("./assets/scan1.txt");
         let mut scan2 = parse_scan("./assets/scan2.txt");
-        let scan2_orig = scan2.clone();      
-        
+        let scan2_orig = scan2.clone();
+
         let res = func(&mut scan1, &mut scan2);
         plot(&scan1, &scan2, &scan2_orig, res, format!("./assets/{name}.svg")).unwrap();
     }
@@ -218,11 +219,11 @@ mod test{
                 split.next().unwrap().parse().unwrap(),
                 split.next().unwrap().parse().unwrap(),
             );
-    
+
             p
         }).collect()
     }
-   
+
     fn plot(
         points1: &[na::Point2<f32>],
         points2: &[na::Point2<f32>],
@@ -231,35 +232,35 @@ mod test{
         name: String,
     ) -> Result<(), Box<dyn std::error::Error>> {
         let [x, y, rot,convergence] = translation;
-    
+
         let root = SVGBackend::new(&name, (720, 720)).into_drawing_area();
         root.fill(&WHITE)?;
-    
+
         // Split the drawing area into two
         let (upper, lower) = root.split_vertically(360);
-    
+
         // Create the first chart
         let mut chart1 = ChartBuilder::on(&upper)
             .margin(5)
             .x_label_area_size(30)
             .y_label_area_size(30)
             .build_cartesian_2d(-4f32..4f32, -4f32..4f32)?;
-    
+
         chart1.configure_mesh().draw()?;
         chart1.draw_series(points1.iter().map(|p| Circle::new((p.x, p.y), 1, BLUE.filled())))?;
         chart1.draw_series(points2_orig.iter().map(|p| Circle::new((p.x, p.y), 1, RED.filled())))?;
-    
+
         // Create the second chart
         let mut chart2 = ChartBuilder::on(&lower)
             .margin(5)
             .x_label_area_size(30)
             .y_label_area_size(30)
             .build_cartesian_2d(-4f32..4f32, -4f32..4f32)?;
-    
+
         chart2.configure_mesh().draw()?;
         chart2.draw_series(points1.iter().map(|p| Circle::new((p.x, p.y), 1, BLUE.filled())))?;
         chart2.draw_series(points2.iter().map(|p| Circle::new((p.x, p.y), 1, RED.filled())))?;
-    
+
         // Add translation information as a label
 
         lower.draw_text(
@@ -267,11 +268,11 @@ mod test{
             &("sans-serif", 20, &BLACK).into_text_style(&lower),
             (50, 5),
         )?;
-    
+
         // Finish the drawing
         root.present()?;
         println!("Plot saved to {}", name);
-    
+
         Ok(())
     }
 }
